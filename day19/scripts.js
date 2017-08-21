865de45ceaf0872a9d3ec6e3557ceb3317cdaf1a
Muhammad D. Ramadhan <muhammaddeni90@gmail.com>
2017-08-21 21:05:22 +0700

    Delete unnecessary code.

--- day19/scripts.js
+++ day19/scripts.js
@@ -5,7 +5,44 @@
   var snap = document.querySelector('.snap');
   var context = canvas.getContext('2d');
   var takePhotoButton = document.querySelector('.take-photo-js');
-  var photos = document.querySelector('.strip');
+  var timeout;
+
+  // It's copy-pasted from internet, but I can explain this
+  // interesting function in my own words.
+  // It's well-explained by John Dugan http://bit.ly/2troSTT
+  function debounce(func, wait = 20, immediate = true) {
+    // It will store unique ID (number) that `window.setTimeout` return.
+    // For more about 'window.setTimeout', visit https://mzl.la/2uuQs7Z
+    var timeout;
+
+    return function() {
+      // In this case, `this` has reference to `video` object and
+      // `arguments` is an array-like object that includes event
+      // the `video` object listen to ('canplay').
+      var context = this, args = arguments;
+
+      // This condition make function being passed to `debounce`
+      // function (`func`) run without having to wait as long as
+      // `timeout` is cleared away. So, the first time `debounce`
+      // return this anonymous function and have it listen to 'canplay'
+      // event, `func` will run immediately.
+      var callNow = immediate && !timeout;
+
+      // This function will fire after we have waited for certain amount
+      // of time. `wait` parameter contains that duration.
+      function later() {
+        // Nullify the variable that stores unique ID (number) after the
+        // timeout passed.
+        timeout = null;
+
+        // If we set `func` not to run immediately after `debounce` being
+        // called, run it anyway after the timeout passed.
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      }
+    };
+  }
 
   function getVideo() {
     navigator.mediaDevices.getUserMedia({
@@ -30,11 +67,11 @@
     context.translate(width, 0);
     context.scale(-1, 1);
 
-    return window.setInterval(function () {
+    clearTimeout(timeout);
+
+    timeout = window.setInterval(function () {
       context.drawImage(video, 0, 0, width, height);
-      pixels = context.getImageData(0, 0, width, height);
-      context.putImageData(pixels, 0, 0);
-    }, 32);
+    }, 16);
   }
 
   function takePhoto() {
@@ -49,7 +86,7 @@
     link.href = data;
     link.setAttribute('download', `handsome-${numbering}.jpeg`);
 
-    link.innerHTML = `<img src="${data}" alt="I am beautiful no matter what they say. Words can't bring me down." />`;
+    link.innerHTML = `<img src="${data}" alt="photo" />`;
     strip.insertBefore(link, strip.firsChild);
   }
 
