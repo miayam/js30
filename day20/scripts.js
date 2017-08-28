733b05525b79862e3f7641283a3d3f10f065f2cb
Muhammad D. Ramadhan <muhammaddeni90@gmail.com>
2017-08-28 07:11:02 +0700

    Change the code for day 20 JavaScript challenge.

--- day20/scripts.js
+++ day20/scripts.js
@@ -1,31 +1,13 @@
 (function (window, undefined) {
   var words = document.querySelector('.words');
   var synthesis = window.speechSynthesis;
-  var wordsArray;
-  var lastWord;
-  var utterance;
-
-  // It's actually from Stack Overflow. Remove empty
-  // element like "", null or undefined from
-  // array.
-  function cleanArray(actual) {
-    var newArray = [];
-    for (var i = 0; i < actual.length; i++) {
-      if (actual[i]) {
-        newArray.push(actual[i]);
-      }
-    }
-    return newArray;
-  }
+  var utterance; 
 
   words.addEventListener('keyup', function (event) {
-    // Utter the last typed word if we happend to type 'space'
-    // key after that.
-    if (event.keyCode === 32) {
-      wordsArray = words.textContent.split(' ');
-      cleanedArray = cleanArray(wordsArray);
-      lastWord = cleanedArray[cleanedArray.length - 1];
-      utterance = new SpeechSynthesisUtterance(lastWord);
+
+    // Utter the words if we happend to type 'CTRL' key.
+    if (event.keyCode === 17) {
+      utterance = new SpeechSynthesisUtterance(words.textContent);
       synthesis.speak(utterance);
     }
   });
